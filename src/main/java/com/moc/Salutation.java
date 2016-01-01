b27135bca86588b5447e63663e4c460fc11eb907
adamr2589 <adamr2589@wp.pl>
2016-01-01 18:42:18 +0100

    second

---
+++ src/main/java/com/moc/Salutation.java
@@ -0,0 +1,16 @@
+package com.moc;
+
+public class Salutation {
+
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}
